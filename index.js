ff07f55625280d756497786ba4389f7e58d2edb3
danielferguson <107422624+danielalexis1860@users.noreply.github.com>
2025-12-11 01:14:52 +0400

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}]: ${req.method} ${req.url}`);
+  next();
+});
