4092e5a0405b34ddbf0eae8911377d90c2a78e49
danielferguson <107422624+danielalexis1860@users.noreply.github.com>
2025-12-11 01:14:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3010;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
 const DB_NAME = 'digital_academy';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
